e9d92a13bece54a72b1209ecfd2a5ae4a36b54d0
warhamhayat <warhamhayat@gmail.coom>
2023-10-17 19:08:34 +0800

    modify main js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import {web} from "./application/web.js";
 import {logger} from "./application/logging.js";
+import { prismaClient } from "./application/database.js";
 
-web.listen(3000, () => {
-    logger.info("App start");
-});
+
+const start = async ()=>{
+    try {
+        await prismaClient.$connect()
+        logger.info("database prisma terhubung")
+        web.listen(3000, () => {
+            logger.info("App start");
+        });
+    } catch (error) {
+        logger.info(error)
+    }
+}
+  
+start();
